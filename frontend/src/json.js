b79477d23a410456772d15e2186d46157fa1fae6
תומי ירמיה <tommyirmia@gmail.com>
2021-09-22 19:21:18 +0300

    added scss files + js files2

---
+++ frontend/src/json.js
@@ -0,0 +1,44 @@
+// Guidlines:
+// *. no need for song store, it is part of the station
+
+var playlist = {
+  "_id": "5c09",
+  "name": "Funky Monks",
+  "tags": [
+    "Funk",
+    "Happy"
+  ],
+  "createdAt": 1541652422,
+  "createdBy": {
+    "_id": "u101",
+    "fullname": "Puki Ben David",
+    "imgUrl": "http://some-photo/"
+  },
+  "likedByUsers": ['{minimal-user}', '{minimal-user}'],
+  "songs": [
+    {
+      "id": "s1001",
+      "title": "The Meters - Cissy Strut",
+      "url": "youtube/song.mp4",
+      "imgUrl": "https://i.ytimg.com/vi/4_iC0MyIykM/mqdefault.jpg",
+      "addedBy": '{minimal-user}'
+    },
+    {
+      "id": "mUkfiLjooxs",
+      "title": "The JB's - Pass The Peas",
+      "url": "youtube/song.mp4",
+      "imgUrl": "https://i.ytimg.com/vi/mUkfiLjooxs/mqdefault.jpg",
+      "addedBy": {}
+    }
+  ]
+}
+
+const user = {
+  "_id" : "xxxxxxx",
+  "fullname" : "woohoooh",
+  "password" : "1234",
+  "email" : "1341234@gmasil.com",
+  "createdPlaylists" : ['5c09' , '423423', 'asd23'],
+  "favoritePlaylists" : ['sdaasd', 'asdasdasd', 'asdgs'],
+  "imgUrl": "http://some-img.jpg",
+}
