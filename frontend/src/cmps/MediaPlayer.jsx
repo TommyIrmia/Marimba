04b86d3d1d6c4febed8de3ffd0ccb8a717822096
תומי ירמיה <tommyirmia@gmail.com>
2021-09-23 23:33:30 +0300

    media player actions

--- frontend/src/cmps/MediaPlayer.jsx
+++ frontend/src/cmps/MediaPlayer.jsx
@@ -3,45 +3,63 @@ import YouTube from 'react-youtube';
 import logo from '../assets/imgs/logo3.png';
 
 export class MediaPlayer extends Component {
+    station = ['CHekNnySAfM', 'S5FCdx7Dn0o', 'JhwxTen6yA', '9ZpxaeICYyg']
 
     state = {
         ev: null,
-        isPlaying: false
+        isPlaying: false,
+        currSongIdx: 0
     }
 
-
     onReady = (ev) => {
+        if (ev.data === 2) return
         this.setState({ ev })
     }
 
-    onTogglePlay = () => {
-        if (this.state.isPlaying) {
-            this.state.ev.target.pauseVideo()
-            this.setState({ isPlaying: false })
-        } else {
+    onChangeSong = (diff, currIdx) => {
+        let nextIdx = currIdx + diff;
+        if (nextIdx < 0) {
+            this.state.ev.target.stopVideo()
             this.state.ev.target.playVideo()
-            this.setState({ isPlaying: true })
+            nextIdx = 0;
+        };
+        if (nextIdx >= this.station.length) nextIdx = 0;
+        this.setState({ currSongIdx: nextIdx })
+    }
+
+    onStateChange = (ev) => {
+        if (ev.data === 2 || !this.state.isPlaying) return
+        ev.target.playVideo()
+    }
+
+    onTogglePlay = (ev) => {
+        if (this.state.isPlaying) {
+            this.setState({ isPlaying: false }, () => {
+                this.state.ev.target.pauseVideo()
+            })
+        } else {
+            this.setState({ isPlaying: true }, () => {
+                this.state.ev.target.playVideo()
+            })
         }
-        console.log(this.state.ev.target)
     }
 
     render() {
-        const { isPlaying } = this.state
+        const { isPlaying, currSongIdx } = this.state
+        console.log(currSongIdx);
         return (
             <div className="media-player">
                 <YouTube
-                    ref={this.youtubeRef}
-                    videoId={'CHekNnySAfM'}                  // defaults -> null
-                    id={'CHekNnySAfM'}                       // defaults -> null
+                    videoId={this.station[currSongIdx]}                  // defaults -> null
+                    id={this.station[currSongIdx]}                       // defaults -> null
                     className={'youtube-player'}                // defaults -> null
                     containerClassName={'player-container'}       // defaults -> ''
                     onReady={this.onReady}                    // defaults -> noop
-                // playSong={ }
-                // onPlay={func}                     // defaults -> noop
-                // onPause={func}                    // defaults -> noop
-                // onEnd={func}                      // defaults -> noop
-                // onError={func}                    // defaults -> noop
-                // onStateChange={func}              // defaults -> noop
+                    // onPlay={func}                     // defaults -> noop
+                    // onPause={func}                    // defaults -> noop
+                    onEnd={() => this.onChangeSong(1, currSongIdx)}                      // defaults -> noop
+                    // onError={func}                    // defaults -> noop
+                    onStateChange={(ev) => this.onStateChange(ev)}      // defaults -> noop
                 // onPlaybackRateChange={func}       // defaults -> noop
                 // onPlaybackQualityChange={func}    // defaults -> noop
                 />
@@ -60,11 +78,16 @@ export class MediaPlayer extends Component {
 
                 <div className="player-actions flex">
                     <button className="action-btn fas fa-random"></button>
-                    <button className="action-btn fas fa-step-backward"></button>
+                    <button className="action-btn fas fa-step-backward"
+                        onClick={() => this.onChangeSong(-1, currSongIdx)}></button>
+
                     <button className={"play-btn " + (isPlaying ? "fas fa-pause" : "fas fa-play")}
                         onClick={this.onTogglePlay}>
                     </button>
-                    <button className="action-btn fas fa-step-forward"></button>
+
+                    <button className="action-btn fas fa-step-forward"
+                        onClick={() => this.onChangeSong(1, currSongIdx)}></button>
+
                     <button className="action-btn fas fa-redo"></button>
 
                     <div className="progress-bar"></div>
