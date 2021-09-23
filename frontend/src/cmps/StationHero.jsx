bc64aee455569a3e18731c76fbbfee3ae7b8e58c
tomer morad <tomer.morad771@gmail.com>
2021-09-23 22:15:34 +0300

    add scss

--- frontend/src/cmps/StationHero.jsx
+++ frontend/src/cmps/StationHero.jsx
@@ -1,19 +1,21 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+// import PersonOutlineIcon from '@material-ui/icons/PersonOutline';
 import { Logo } from './Logo'
 
 export class StationHero extends Component {
     render() {
         return (
             <div className="StationHero">
-                <div className="img-container"><Logo className="logo"/></div>
+                
+                <div className="img-container"><Logo className="logo" /></div>
                 <div className="info-container">
                     <h5>playlist</h5>
                     <h1 className="hero-title">Cheesy Hits!</h1>
                     <p>discription</p>
-                    <Link to="/" ><Logo className="logo"/></Link>
-                    
+                    <Link to="/" ><Logo className="logo" /></Link>
+
                 </div>
             </div>
         )
--- frontend/src/pages/StationDetails.jsx
+++ frontend/src/pages/StationDetails.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
 import { StationHero } from './../cmps/StationHero';
+import { StationActions } from './../cmps/StationActions';
 
 export class StationDetails extends Component {
     render() {
         return (
             <section className="StationDetails">
             <StationHero/>
+            <StationActions/>
             <h3>s</h3>
             <h3>s</h3>
             <h3>s</h3>
