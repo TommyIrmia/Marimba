c7c11f38195366936e6959caa5deff157ebc7b8e
naamaark <naamaark@gmail.com>
2021-09-23 16:25:47 +0300

    track service

---
+++ frontend/src/services/async-storage.service.js
@@ -0,0 +1,70 @@
+
+export const storageService = {
+    query,
+    get,
+    post,
+    put,
+    remove,
+    save
+}
+
+function query(entityType, delay = 1200) {
+    var entities = JSON.parse(localStorage.getItem(entityType)) || []
+
+    return new Promise((resolve, reject)=>{
+        setTimeout(()=>{
+            // reject('OOOOPs')
+            resolve(entities)
+        }, delay)   
+    })
+    // return Promise.resolve(entities)
+}
+
+
+function get(entityType, entityId) {
+    return query(entityType)
+        .then(entities => entities.find(entity => entity._id === entityId))
+}
+function post(entityType, newEntity) {
+    newEntity._id = _makeId()
+    return query(entityType)
+        .then(entities => {
+            entities.push(newEntity)
+            save(entityType, entities)
+            return newEntity
+        })
+}
+
+function put(entityType, updatedEntity) {
+    return query(entityType)
+        .then(entities => {
+            const idx = entities.findIndex(entity => entity._id === updatedEntity._id)
+            entities.splice(idx, 1, updatedEntity)
+            save(entityType, entities)
+            return updatedEntity
+        })
+}
+
+function remove(entityType, entityId) {
+    return query(entityType)
+        .then(entities => {
+            const idx = entities.findIndex(entity => entity._id === entityId)
+            entities.splice(idx, 1)
+            save(entityType, entities)
+        })
+}
+
+
+function save(entityType, entities) {
+    console.log('entityType FROM SAVE!', entityType)
+    localStorage.setItem(entityType, JSON.stringify(entities))
+}
+
+function _makeId(length = 5) {
+    var text = ''
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+    for (var i = 0; i < length; i++) {
+        text += possible.charAt(Math.floor(Math.random() * possible.length))
+    }
+    return text
+}
---
+++ frontend/src/services/track.service.js
@@ -0,0 +1,55 @@
+
+import { storageService } from './async-storage.service.js'
+var initialTracks =
+    [
+        {
+            "id": "A_MjCqQoLLA",
+            "title": "Hey Jude- Beatels",
+            "url": "youtube/song.mp4",
+            "imgUrl": "https://i.ytimg.com/vi/4_iC0MyIykM/mqdefault.jpg",
+            "addedBy": 'Naama'
+        },
+        {
+            "id": "m2uTFF_3MaA",
+            "title": "Yellow Submarine- Beatels",
+            "url": "youtube/song.mp4",
+            "imgUrl": "https://i.ytimg.com/vi/mUkfiLjooxs/mqdefault.jpg",
+            "addedBy": 'Tomer'
+        }
+    ]
+
+const STORAGE_KEY = 'track'
+
+export const trackService = {
+    query,
+    getById,
+    add,
+    remove
+}
+
+
+function query() {
+    const tracks = storageService.query(STORAGE_KEY).then(tracks => {
+        if (tracks.length) return tracks
+        else {
+            _saveTracksToStorage()
+            return initialTracks
+        }
+
+    })
+    return Promise.resolve(tracks)
+}
+function getById(trackId) {
+    return storageService.get(STORAGE_KEY, trackId)
+}
+function remove(trackId) {
+    // return Promise.reject('Not now!');
+    return storageService.remove(STORAGE_KEY, trackId)
+}
+function add(track) {
+        return storageService.post(STORAGE_KEY, track)
+}
+
+function _saveTracksToStorage() {
+    storageService.save(STORAGE_KEY, initialTracks)
+}
