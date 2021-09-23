c8fb9a54cfc2dbb1aa090e09775adbd7b9dcdab1
tomer morad <tomer.morad771@gmail.com>
2021-09-24 00:18:09 +0300

    add action buttons

--- frontend/src/cmps/StationActions.jsx
+++ frontend/src/cmps/StationActions.jsx
@@ -2,10 +2,19 @@ import React, { Component } from 'react'
 
 export  class StationActions extends Component {
     render() {
+        const isPlaying=false;
         return (
-            <div className="StationActions">
-                actions
-            </div>
+            <main className="actions-container">
+            <section className="StationActions playlist-layout">
+                <div className="btns-actions flex">
+                <button className={"play-btn " + (isPlaying ? "fas fa-pause" : "fas fa-play")}>
+                    </button>
+                    <button className="far fa-heart btn-action"></button>
+                    <button className="far fa-arrow-alt-circle-down btn-action"></button>
+                    <button className="fas fa-ellpsis-h btn-action"></button>
+                </div>
+            </section>
+            </main>
         )
     }
 }
