556ed517caee83e5ab3853f2d1a7f66c9ffd18de
tomer morad <tomer.morad771@gmail.com>
2021-09-24 01:44:51 +0300

    add filter

--- frontend/src/cmps/StationActions.jsx
+++ frontend/src/cmps/StationActions.jsx
@@ -1,19 +1,27 @@
 import React, { Component } from 'react'
 
-export  class StationActions extends Component {
+export class StationActions extends Component {
     render() {
-        const isPlaying=false;
+        const isPlaying = false;
+        const isInputShown = true;
         return (
             <main className="actions-container">
-            <section className="StationActions playlist-layout">
-                <div className="btns-actions flex">
-                <button className={"play-btn " + (isPlaying ? "fas fa-pause" : "fas fa-play")}>
-                    </button>
-                    <button className="far fa-heart btn-action"></button>
-                    <button className="far fa-arrow-alt-circle-down btn-action"></button>
-                    <button className="fas fa-ellpsis-h btn-action"></button>
-                </div>
-            </section>
+                <section className="StationActions playlist-layout">
+                    <div className="btns-actions flex">
+                        <button className={"play-btn " + (isPlaying ? "fas fa-pause" : "fas fa-play")}>
+                        </button>
+                        <button className="far fa-heart btn-action"></button>
+                        <button className="far fa-arrow-alt-circle-down btn-action"></button>
+                        <button className="fas fa-ellpsis-h btn-action"></button>
+                    </div>
+                    <div className="filter-container">
+                    <button className={"fas fa-search btn-search " + (isInputShown ? "invisible" : "")}></button>
+                    {/* <button  className="fas fa-search btn-search" ></button> */}
+                    <button className="fas fa-sort-down btn-sort" >Custom order</button>
+                    <input  className={(isInputShown ? "show" : "unshow")} 
+                    type="text" placeholder="Search in playlist"/>
+                    </div>
+                </section>
             </main>
         )
     }
