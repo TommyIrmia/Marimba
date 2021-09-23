888990532904cdca34c4c56da5f1a4f457dbc70a
naamaark <naamaark@gmail.com>
2021-09-23 13:13:57 +0300

    hidden youtube player

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,16 +3,16 @@ export const App = () => {
 
   return (
     <div className="App">
-      {/* <div className="player-container">
-      <iframe frameborder="0"
-       allowfullscreen="1" 
+      <div className="player-container">
+      <iframe frameBorder="0"
+       allowFullScreen="1" 
        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
        title="YouTube video player" 
        width="100%" 
        height="100%" 
        src="https://www.youtube.com/embed/CHekNnySAfM?autoplay=1&amp;mute=0&amp;controls=0&amp;origin=https%3A%2F%2Fca-beatbox.herokuapp.com&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;enablejsapi=1&amp;widgetid=3" 
        id="widget4"></iframe>
-      </div> */}
+      </div>
 
       <h1>Marimba</h1>
       <h2>Marimba</h2>
