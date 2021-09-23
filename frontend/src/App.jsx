cf66c21b1a747722ce6787d50b8680f71dd21c14
תומי ירמיה <tommyirmia@gmail.com>
2021-09-23 13:21:26 +0300

    added logo

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,20 +4,22 @@ export const App = () => {
 
   return (
     <div className="App">
+      <Logo />
+
       <div className="player-container">
-      <iframe frameBorder="0"
-       allowFullScreen="1" 
-       allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-       title="YouTube video player" 
-       width="100%" 
-       height="100%" 
-       src="https://www.youtube.com/embed/CHekNnySAfM?autoplay=1&amp;mute=0&amp;controls=0&amp;origin=https%3A%2F%2Fca-beatbox.herokuapp.com&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;enablejsapi=1&amp;widgetid=3" 
-       id="widget4"></iframe>
+        <iframe frameBorder="0"
+          allowFullScreen="1"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          title="YouTube video player"
+          width="100%"
+          height="100%"
+          src="https://www.youtube.com/embed/CHekNnySAfM?autoplay=1&amp;mute=0&amp;controls=0&amp;origin=https%3A%2F%2Fca-beatbox.herokuapp.com&amp;playsinline=1&amp;showinfo=0&amp;rel=0&amp;iv_load_policy=3&amp;modestbranding=1&amp;enablejsapi=1&amp;widgetid=3"
+          id="widget4"></iframe>
       </div>
 
       <a>Based on your recent listening</a>
     </div>
-    
+
   );
 }
 
