4bb2c50f2ac5652db4fdda4578ed6ccfc040ba5d
tomer morad <tomer.morad771@gmail.com>
2021-09-24 13:09:29 +0300

    add sort

--- frontend/src/pages/StationDetails.jsx
+++ frontend/src/pages/StationDetails.jsx
@@ -3,11 +3,38 @@ import { StationHero } from './../cmps/StationHero';
 import { StationActions } from './../cmps/StationActions';
 
 export class StationDetails extends Component {
+
+    state={
+         isSearch: false,
+    }
+
+    inputRef = React.createRef()
+
+    onSearch = () => {
+        this.inputRef.current.focus()
+        this.setState({ isSearch: true });
+    }
+
+    onCloseSerach = () => {
+        this.setState({ isSearch: false });
+    }
+
+   
+
+
     render() {
+        const {isSearch} = this.state;
         return (
             <section className="StationDetails">
+                <div onClick={this.onCloseSerach} className={(isSearch ? "screen" : "")}></div>
                 <StationHero />
-                <StationActions />
+                <StationActions inputRef={this.inputRef} onSearch={this.onSearch} isSearch={isSearch} />
+                <h3>s</h3>
+                <h3>s</h3>
+                <h3>s</h3>
+                <h3>s</h3>
+                <h3>s</h3>
+                <h3>s</h3>
                 <h3>s</h3>
                 <h3>s</h3>
                 <h3>s</h3>
