6d5969a74f05c45f5512cb8c4989079f05ef55ea
naamaark <naamaark@gmail.com>
2021-09-24 17:02:01 +0300

    design suggest preview 1

--- frontend/src/cmps/SuggestTrackPreview.jsx
+++ frontend/src/cmps/SuggestTrackPreview.jsx
@@ -1,11 +1,13 @@
 
 export function SuggestTrackPreview({ track, onAddTrack }) {
+    console.log('img url:', track.imgUrl);
     return (
-        <div className="suggest-track">
-            <p>
-                <span>{track.title}</span>
+        <div className="SuggestTrack">
+            <span>
+            <span>{track.title}</span>
+           <img src={track.imgURL} alt='track'/>
+            </span>
                 <button onClick={()=>onAddTrack(track)}>Add</button>
-            </p>
         </div>
     )
 }
