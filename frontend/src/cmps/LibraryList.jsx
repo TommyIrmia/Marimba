3fbf715666efa518aebe1351d749270e5fce6d93
tomer morad <tomer.morad771@gmail.com>
2021-10-03 12:41:56 +0300

    add

--- frontend/src/cmps/LibraryList.jsx
+++ frontend/src/cmps/LibraryList.jsx
@@ -3,9 +3,8 @@ import { Link } from 'react-router-dom'
 import { StationPreview } from './StationPreview';
 
 export function LibraryList({ likedStations, likedTracks, stationsBy,recentlyStations }) {
-    console.log('recentlyStations⭐⭐⭐',recentlyStations);
     return (
-        <section className="LibraryList grid">
+        <section className="LibraryList flex">
 
             {likedTracks && <Link to="/station/liked" className="liked-track-container" >
                 <div className="tracks-title">
@@ -18,17 +17,17 @@ export function LibraryList({ likedStations, likedTracks, stationsBy,recentlySta
                 </div>
             </Link>}
 
-            <div className="grid relative station" >
+            <div className="flex relative station" >
                 <h3 className="list-title" >Recently added</h3>
                 {recentlyStations.map(recentlyStation => <StationPreview  key={recentlyStation._id} station={recentlyStation} />)}
             </div>
 
-            <div className="grid relative station created-by" >
+            <div className="flex relative station created-by" >
                 <h3 className="list-title" >You created</h3>
                 {stationsBy.map(stationBy => <StationPreview  key={stationBy._id} station={stationBy} />)}
             </div>
 
-            <div className="grid relative station" >
+            <div className="flex relative station" >
                 <h3 className="list-title" >You liked</h3>
                 {likedStations.map(likedStation => <StationPreview  key={likedStation._id} station={likedStation} />)}
             </div>
