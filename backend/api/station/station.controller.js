587fa6e93b3b6a3e0e0d629a765c7b05443cde67
naamaark <naamaark@gmail.com>
2021-10-05 19:58:09 +0300

    backend!!

---
+++ backend/api/station/station.controller.js
@@ -0,0 +1,77 @@
+const stationService = require('./station.service')
+const socketService = require('../../services/socket.service')
+const logger = require('../../services/logger.service')
+
+async function getStation(req, res) {
+    try {
+        const station = await stationService.getById(req.params.id)
+        res.send(station)
+    } catch (err) {
+        logger.error('Failed to get station', err)
+        res.status(500).send({ err: 'Failed to get station' })
+    }
+}
+
+async function getStations(req, res) {
+    try {
+        const filterBy = {
+            txt: req.query?.name || ''
+        }
+        const stations = await stationService.query(filterBy)
+        res.send(stations)
+    } catch (err) {
+        logger.error('Failed to get stations', err)
+        res.status(500).send({ err: 'Failed to get stations' })
+    }
+}
+
+async function getGenres(req, res) {
+    try {
+        const genres = await stationService.getGenres()
+        res.send(genres)
+    } catch (err) {
+        logger.error('Failed to get genres', err)
+        res.status(500).send({ err: 'Failed to get genres' })
+    }
+}
+
+async function deleteStation(req, res) {
+    try {
+        await stationService.remove(req.params.id)
+        res.send({ msg: 'Deleted successfully' })
+    } catch (err) {
+        logger.error('Failed to delete station', err)
+        res.status(500).send({ err: 'Failed to delete station' })
+    }
+}
+
+async function updateStation(req, res) {
+    try {
+        const station = req.body
+        const savedStation = await stationService.update(station)
+        res.send(savedStation)
+    } catch (err) {
+        logger.error('Failed to update station', err)
+        res.status(500).send({ err: 'Failed to update station' })
+    }
+}
+
+async function addStation(req, res) {
+    try {
+        const station = req.body
+        await stationService.add(station)
+        res.send(station)
+    } catch (err) {
+        logger.error('Failed to add station', err)
+        res.status(500).send({ err: 'Failed to update station' })
+    }
+}
+
+module.exports = {
+    getStation,
+    getStations,
+    deleteStation,
+    updateStation,
+    addStation,
+    getGenres
+}
---
+++ backend/api/station/station.service.js
@@ -0,0 +1,105 @@
+
+const dbService = require('../../services/db.service')
+const logger = require('../../services/logger.service')
+const ObjectId = require('mongodb').ObjectId
+
+module.exports = {
+    query,
+    getById,
+    remove,
+    update,
+    add
+}
+
+async function query(filterBy = {}) {
+    console.log('query in back, filterBy', filterBy);
+    const criteria = _buildCriteria(filterBy)
+    try {
+        const collection = await dbService.getCollection('station')
+        var stations = await collection.find(criteria).toArray()
+        stations = stations.map(station => {
+            station.createdAt = ObjectId(station._id).getTimestamp()
+            // Returning fake fresh data
+            // station.createdAt = Date.now() - (1000 * 60 * 60 * 24 * 3) // 3 days ago
+            return station
+        })
+        return stations
+    } catch (err) {
+        logger.error('cannot find stations', err)
+        throw err
+    }
+}
+
+async function getById(stationId) {
+    try {
+        const collection = await dbService.getCollection('station')
+        const station = await collection.findOne({ '_id': ObjectId(stationId) })
+        return station
+    } catch (err) {
+        logger.error(`while finding station ${stationId}`, err)
+        throw err
+    }
+}
+
+async function remove(stationId) {
+    try {
+        const collection = await dbService.getCollection('station')
+        await collection.deleteOne({ '_id': ObjectId(stationId) })
+    } catch (err) {
+        logger.error(`cannot remove station ${stationId}`, err)
+        throw err
+    }
+}
+
+async function update(station) {
+    try {
+        // peek only updatable fields!
+        const stationToSave = {
+            _id: ObjectId(station._id), // needed for the returnd obj
+            name: station.name,
+            description: station.description,
+            bgc: station.bgc,
+            tags: station.tags,
+            imgUrl: station.imgUrl,
+            likedByUsers: station.likedByUsers,
+            tracks: station.tracks
+        }
+        const collection = await dbService.getCollection('station')
+        await collection.updateOne({ _id: stationToSave._id }, { $set: stationToSave })
+        return stationToSave;
+    } catch (err) {
+        logger.error(`cannot update station ${station._id}`, err)
+        throw err
+    }
+}
+
+async function add(station) {
+    try {
+        const collection = await dbService.getCollection('station')
+        await collection.insertOne(station)
+        return station
+    } catch (err) {
+        logger.error('cannot insert station', err)
+        throw err
+    }
+}
+
+function _buildCriteria(filterBy) {
+    const criteria = {}
+    if (filterBy.txt) {
+        const txtCriteria = { $regex: filterBy.txt, $options: 'i' }
+        criteria.$or = [
+            {
+                name: txtCriteria
+            },
+            {
+                genre: txtCriteria
+            }
+        ]
+    }
+    return criteria
+}
+
+
+
+
